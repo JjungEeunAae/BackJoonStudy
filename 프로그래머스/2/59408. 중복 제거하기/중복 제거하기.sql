9a661b06aa5e7b68d424f8aa40910bef8d86d1a0
JjungEeunAae <121069641+JjungEeunAae@users.noreply.github.com>
2023-11-02 16:44:53 +0900

    Update 중복 제거하기.sql

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
 SELECT COUNT(*)
 FROM (
-       SELECT DISTINCT NAME
+       SELECT DISTINCT NAME   -- DISTINCT로 중복 제거
        FROM ANIMAL_INS
-       WHERE NAME IS NOT NULL
+       WHERE NAME IS NOT NULL -- NAME이 NULL인건 제외
      );
