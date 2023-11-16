dc88aa0673c38b9ab91492844dbc0bac56435fc4
JjungEeunAae <121069641+JjungEeunAae@users.noreply.github.com>
2023-11-16 23:47:29 +0900

    [Bronze V] Title: A+B - 8, Time: 180 ms, Memory: 16388 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,18 +1,29 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String args[]) {
-
-		Scanner sc = new Scanner(System.in);
-
-		int a = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		// InputStream : 자바의 가장 기본이 되는 입력 스트림
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// OutputStreamWriter : 자바의 가장 기본이 되는 출력 스트림
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int a = Integer.parseInt(br.readLine());
+		
+		StringTokenizer str;
 
 		for(int i = 1 ; i <= a ; i++) {
-			int b = sc.nextInt();
-			int c = sc.nextInt();
-			System.out.println("Case #" + i + ": "+ b + " + " + c + " = " + (b+c));
-		}
+			str = new StringTokenizer(br.readLine()," ");
+			
+			int b = Integer.parseInt(str.nextToken());
+			int c = Integer.parseInt(str.nextToken());
+			
+			bw.write("Case #" + i + ": "+ b + " + " + c + " = " + (b + c) + "\n");
+		};
 		
+		br.close();
+		
+		bw.flush();
+		bw.close();
 	}
 }
