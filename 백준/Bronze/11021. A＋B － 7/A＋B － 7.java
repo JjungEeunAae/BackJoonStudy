1ee07ca1bf99fa1024dbe3fee15a7cb10783cab1
JjungEeunAae <121069641+JjungEeunAae@users.noreply.github.com>
2023-11-16 17:01:57 +0900

    [Bronze V] Title: A+B - 7, Time: 152 ms, Memory: 16072 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,25 +1,25 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		// InputStream : 자바의 가장 기본이 되는 입력 스트림
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// OutputStreamWriter : 자바의 가장 기본이 되는 출력 스트림
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		// Stream 이란 출발지와 도착지를 이어주는 다리다.
 		
-		// readLine() 메서드는 반환되는 타입이 String.
-		// 입력한 값을 int로 변환하기 위해 Integer.parseInt()를 사용함. 
+		// 입력 값을 String에서 int로 변환.
 		int a = Integer.parseInt(br.readLine());
+		
+		// 문자열 분리 담당
+		StringTokenizer str;
 
 		for(int i = 1 ; i <= a ; i++) {
+			str = new StringTokenizer(br.readLine(), " ");
 			
-			String str = br.readLine();
-			
-			int b = Integer.parseInt(str.split(" ")[0]);
-			
-			int c = Integer.parseInt(str.split(" ")[1]);
+			int b = Integer.parseInt(str.nextToken());
+			int c = Integer.parseInt(str.nextToken());
 			
 			bw.write("Case #" + i + ": " + (b + c) + "\n");
 		}
