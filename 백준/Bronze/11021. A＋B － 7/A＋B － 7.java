b8f62cd5e5b5110f0e7c4ec9a6fa050c2e0ed136
JjungEeunAae <121069641+JjungEeunAae@users.noreply.github.com>
2023-11-16 16:43:41 +0900

    [Bronze V] Title: A+B - 7, Time: 164 ms, Memory: 16152 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,17 +1,28 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	public static void main(String args[]) {
-
-		Scanner sc = new Scanner(System.in);
-
-		int a = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// InputStream : 자바의 가장 기본이 되는 입력 스트림
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// OutputStreamWriter : 자바의 가장 기본이 되는 출력 스트림
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		// Stream 이란 출발지와 도착지를 이어주는 다리다.
+		
+		// readLine() 메서드는 반환되는 타입이 String.
+		// 입력한 값을 int로 변환하기 위해 Integer.parseInt()를 사용함. 
+		int a = Integer.parseInt(br.readLine());
 
 		for(int i = 1 ; i <= a ; i++) {
-			int b = sc.nextInt();
-			int c = sc.nextInt();
-			System.out.println("Case #"+(i)+": "+(b+c));
+			
+			String str = br.readLine();
+			
+			int b = Integer.parseInt(str.split(" ")[0]);
+			
+			int c = Integer.parseInt(str.split(" ")[1]);
+			
+			System.out.println("Case #"+(i)+": "+(b + c));
+			
 		}
 		
 	}
