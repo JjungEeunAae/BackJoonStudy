ee360ff160fcc873b97601302c51394eecc9452a
JjungEeunAae <121069641+JjungEeunAae@users.noreply.github.com>
2023-11-17 11:49:34 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,22 +1,27 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	public static void main(String args[]) {
-
-		Scanner sc = new Scanner(System.in);
-
-		int n = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		// InputStream : 자바의 가장 기본이 되는 입력 스트림
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// OutputStreamWriter : 자바의 가장 기본이 되는 출력 스트림
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
 		
 		for(int i = 1 ; i <= n ; i++) {
 			for(int j = n-i ; j >= 1 ; j--) {
-				System.out.print(" ");
+				bw.write(" ");
 			}
 			for(int j = 1 ; j <= i ; j++) {
-				System.out.print("*");
+				bw.write("*");
 			}
-			System.out.println();
+			bw.write("\n");
 		}
 		
+		br.close();
+		
+		bw.flush();
+		bw.close();
 	}
 }
