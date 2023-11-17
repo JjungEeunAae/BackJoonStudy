91c9ebad3c5235860c86034b1d9c7e494d6fa18e
JjungEeunAae <121069641+JjungEeunAae@users.noreply.github.com>
2023-11-17 14:52:26 +0900

    [Bronze V] Title: A+B - 4, Time: 144 ms, Memory: 14676 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,16 +1,27 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		// InputStream : 자바의 가장 기본이 되는 입력 스트림
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// OutputStreamWriter : 자바의 가장 기본이 되는 출력 스트림
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-        while(sc.hasNextInt()) {
-        	int A = sc.nextInt();
-        	int B = sc.nextInt();
-        	
-        	System.out.println(A+B);
-        };
-        
-        sc.close();
+		StringTokenizer st;
+		String str;
+		
+		while( (str = br.readLine()) != null) {
+			st = new StringTokenizer(str, " ");
+			
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			bw.write( ( A + B ) + "\n");
+		}
+		br.close();
+		
+		bw.flush();
+		bw.close();
 	}
 }
