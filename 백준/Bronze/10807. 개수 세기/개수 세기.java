eedb33fb548512c99e413bb42bd0d435d25735fc
JjungEeunAae <121069641+JjungEeunAae@users.noreply.github.com>
2023-11-20 15:36:36 +0900

    [Bronze V] Title: 개수 세기, Time: 148 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,25 +1,34 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		// InputStream : 자바의 가장 기본이 되는 입력 스트림
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// OutputStreamWriter : 자바의 가장 기본이 되는 출력 스트림
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int[] arr = new int[sc.nextInt()];
+		int[] arr = new int[Integer.parseInt(br.readLine())];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
 		for(int i = 0 ; i < arr.length ; i++) {
-			int num = sc.nextInt();
 			
-			arr[i] = num;
-		};
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 		
 		int cnt = 0;
-		int v = sc.nextInt();
+		int v = Integer.parseInt(br.readLine());
 		
-		for(int i = 0 ; i < arr.length ; i++) {
-			if(arr[i] == v) {
+		for(int j = 0 ; j < arr.length ; j++) {
+			if(arr[j] == v) {
 				cnt++;
 			}
 		}
-		System.out.println(cnt);
+		br.close();
+
+		bw.write(cnt + " ");
+		
+		bw.flush();
+		bw.close();
 	}
 }
