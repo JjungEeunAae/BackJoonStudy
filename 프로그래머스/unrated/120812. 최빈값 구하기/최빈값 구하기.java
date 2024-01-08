6681156968c29f6f92dffcb3bb7e782300d142ca
JjungEeunAae <121069641+JjungEeunAae@users.noreply.github.com>
2024-01-08 23:21:02 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.05 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,27 +1,31 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] array) {
-        int answer = array[0];          // 배열 중 제일 첫번째 값을 maxNum으로 초기설정
-        int max = 0;                    // 값을 담기 위한 변수 초기화
-        int frequent[] = new int[1000]; // array 길이 초기설정
+        Arrays.sort(array);
+
+        int answer = 0;
+        int max = 0;
+        int maxNum = array[0];
+        int cnt = 1;
+
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] == array[i-1]) {
+                cnt++;
+                if(cnt > max) {
+                    max = cnt;
+                    maxNum = array[i];
+                }
+                else if(cnt == max) {
+                    maxNum = -1;
+                }
+            }
+            else{
+                cnt = 1;
+            }
+        }
         
-        for(int i = 0; i < array.length ; i++) {
-            frequent[array[i]]++;
-            
-            if(max < frequent[array[i]]) {
-                max = frequent[array[i]];
-                answer = array[i];
-            }
-        };
-        
-        int MultipleOrNot = 0;  // 여러개인지 확인하기 위한 변수 선언
-        for(int i = 0 ; i < 1000 ; i++) {
-            if(max == frequent[i]) {
-                MultipleOrNot++;
-            }
-            if(MultipleOrNot > 1) {
-                answer = -1;
-            }
-        };
+        answer = maxNum;
         
         return answer;
     }
